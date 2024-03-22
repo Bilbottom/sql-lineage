c14af250d5cff1f1bad3100b8805bdf0a219e43a
Bill <Bill@TheWallisFamily.co.uk>
2024-03-22 18:39:36 +0000

    ✨ feat: Add parser and mermaid-er

--- sql_lineage/main.py
+++ sql_lineage/main.py
@@ -1,11 +1,52 @@
 """
-Entry point into the package.
+An SQL lineage tool, built on the awesome SQLGlot library.
 """
+
+import sqlglot
+from sqlglot import exp
+from sqlglot.optimizer import qualify
+
+Nodes = list[str]
+Edges = list[tuple[str, str]]  # Directed edges, source --> target.
+
+
+def parse_ctes(sql: str) -> [Nodes, Edges]:
+    """
+    Parse an SQL query into nodes and edges based on CTEs.
+    """
+    parsed = qualify.qualify(sqlglot.parse_one(sql))
+    nodes, edges = [], []
+
+    # Lazy and error-prone CTE sourcing
+    for cte in parsed.find_all(exp.CTE):
+        nodes.append(cte.alias)
+        edges.extend((tbl.name, cte.alias) for tbl in cte.find_all(exp.Table))
+
+    parsed.args["with"] = None  # Dirty hack to remove the CTEs from the query
+    nodes.append("final")
+    edges.extend((tbl.name, "final") for tbl in parsed.find_all(exp.Table))
+
+    return nodes, edges
+
+
+def to_mermaid(nodes: Nodes, edges: Edges) -> str:
+    """
+    Convert nodes and edges into a Mermaid graph.
+    """
+    mermaid = "graph TD\n"
+
+    for node in nodes:
+        mermaid += f"    {node}\n"  # pylint: disable=consider-using-join
+
+    for edge in edges:
+        mermaid += f"    {edge[0]} --> {edge[1]}\n"
+
+    return mermaid
 
 
 def main() -> None:
     """
-    Entry point into the module.
+    Entry point into the SQL lineage tool.
     """
 
 
--- tests/test__main.py
+++ tests/test__main.py
@@ -2,11 +2,74 @@
 Unit tests for the ``sql_lineage.main`` module.
 """
 
+import textwrap
+
+import pytest
+
 import sql_lineage.main as main
 
 
-def test__main(monkeypatch):
+@pytest.fixture
+def nodes() -> main.Nodes:
+    return ["aaa", "bbb", "ccc", "ddd", "final"]
+
+
+@pytest.fixture
+def edges() -> main.Edges:
+    return [
+        ("aaa", "ccc"),
+        ("aaa", "ddd"),
+        ("bbb", "ddd"),
+        ("ccc", "final"),
+        ("ddd", "final"),
+    ]
+
+
+@pytest.fixture
+def sql_with_ctes() -> str:
+    return textwrap.dedent(
+        """
+        with
+            aaa as (select 1 as aa),
+            bbb as (select 2 as bb),
+            ccc as (select 3 as cc, aa from aaa),
+            ddd as (select 4 as dd, aa from aaa where aa not in (select bb from bbb))
+
+        select *
+        from ccc
+            inner join ddd using (aa)
+        """
+    )
+
+
+@pytest.fixture
+def mermaid_with_ctes() -> str:
+    return textwrap.dedent(
+        """\
+        graph TD
+            aaa
+            bbb
+            ccc
+            ddd
+            final
+            aaa --> ccc
+            aaa --> ddd
+            bbb --> ddd
+            ccc --> final
+            ddd --> final
+        """
+    )
+
+
+def test__parse_ctes(nodes: main.Nodes, edges: main.Edges, sql_with_ctes: str):
     """
-    Test the ``main`` function.
+    Test the ``parse_ctes`` function.
     """
-    main.main()
+    assert main.parse_ctes(sql_with_ctes) == (nodes, edges)
+
+
+def test__to_mermaid(nodes: main.Nodes, edges: main.Edges, mermaid_with_ctes: str):
+    """
+    Test the ``to_mermaid`` function.
+    """
+    assert main.to_mermaid(nodes, edges) == mermaid_with_ctes
